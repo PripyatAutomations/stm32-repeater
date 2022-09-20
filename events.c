bebef9518ef2fb2189e82515806116b96b527b6a
Pripyat Automations <84538339+PripyatAutomations@users.noreply.github.com>
2022-09-20 06:08:05 -0400

    cleanup

--- events.c
+++ events.c
@@ -2,3 +2,18 @@
  * Event handler system
  */
 #include "repeater.h"
+
+/* our event-queue */
+rpt_event_t events[MAX_EVENTS];
+
+bool event_check_all(void) {
+   return false;
+}
+
+bool event_register(rpt_event_t *event) {
+   return false;
+}
+
+bool event_delete(rpt_event_t *event) {
+   return false;
+}
--- events.h
+++ events.h
@@ -1,6 +1,8 @@
 #if	!defined(_REPEATER_events_h)
 #define	_REPEATER_events_h
 
+#define	MAX_EVENTS	256
+
 /* */
 typedef struct rpt_event rpt_event_t;
 struct rpt_event {
@@ -8,5 +10,9 @@ struct rpt_event {
     char name[13];			/* 12 char name */
     bool (*callback)(void *arg);	/* callback */
 };
+
+extern bool event_check_all(void);
+extern bool event_register(rpt_event_t *event);
+extern bool event_delete(rpt_event_t *event);
 
 #endif
